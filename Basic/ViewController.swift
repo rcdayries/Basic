3e9f9a7f70064283ebabe918204cde6e279c3551
Richard Dayries <richard@richards-macbook-2.local>
2018-02-13 17:54:45 -0600

    Finished tutorial for 'Basic'

--- Basic/ViewController.swift
+++ Basic/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITextFieldDelegate {
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myTextField.delegate = self
     }
-
+    @IBAction func didTapButton(_ sender: UIButton) {
+        if let name = myTextField.text {
+            myLabel.text = "Hi \(name)!"
+            myTextField.resignFirstResponder()
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let name = myTextField.text {
+            myLabel.text = "Hi \(name)!"
+            myTextField.resignFirstResponder()
+        }
+        return false
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
