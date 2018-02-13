edbfe01f89b85c030d2731417a49cf45bb4440b2
Richard Dayries <richard@richards-macbook-2.local>
2018-02-13 14:01:53 -0600

    Initial Commit

---
+++ Basic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Basic
+//
+//  Created by Richard Dayries on 2/13/18.
+//  Copyright © 2018 org.dayries. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
